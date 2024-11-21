b8b0d4239dbdd73bdb5c8f3ea96c6ab3ebd8fadb
rulofloo <rulofloo777@gmail.com>
2024-11-21 16:57:05 -0700

    Update dna_app.py

--- dna_app.py
+++ dna_app.py
@@ -166,7 +166,7 @@ st.subheader('5. Display Pie Chart')
 import altair as alt
 
 # Reshape the data for animated pie chart
-df_pivot = df.melt('nucleotide', var_name='metric', value_name='value')
+df_pivot = df.melt('nucleotido', var_name='metric', value_name='valor')
 
 # Create animated pie chart
 animated_pie_chart = alt.Chart(df_pivot).mark_arc().encode(
